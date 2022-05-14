4bfd223b8302028c8623f41be1cd951745de5bbf
Simon <770632+Ponjimon@users.noreply.github.com>
2022-05-14 13:07:53 +0200

    Initial commit

---
+++ app/root.tsx
@@ -0,0 +1,32 @@
+import type { MetaFunction } from '@remix-run/cloudflare';
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react';
+
+export const meta: MetaFunction = () => {
+  return { title: 'New Remix App' };
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta content="width=device-width,initial-scale=1" name="viewport" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
